2ce63265c1d847fb2990eb1829c01be22633c2f1
Berkcan Karabulut <46574515+berkcankarabulut@users.noreply.github.com>
2025-11-28 02:55:43 +0300

    Init: Project started. Assets Imported.

---
+++ Assets/_Projects/Scripts/Player/PlayerVehicleController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Game.Player
+{
+    
+    public class PlayerVehicleController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    
+  
+    }
+}
