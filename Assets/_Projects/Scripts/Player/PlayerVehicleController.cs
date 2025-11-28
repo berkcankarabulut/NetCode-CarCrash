aa52b0f60df4e7ef09f6a30122bbd1dd33bfda07
Berkcan Karabulut <46574515+berkcankarabulut@users.noreply.github.com>
2025-11-28 11:15:15 +0300

    v0.4: Player Car Controller completed

--- Assets/_Projects/Scripts/Player/PlayerVehicleController.cs
+++ Assets/_Projects/Scripts/Player/PlayerVehicleController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Game.Player.Data;
 using UnityEngine;
@@ -8,99 +9,114 @@ namespace Game.Player
     {
         private class SpringData
         {
-            public float _currentLength;
-            public float _currentVelocity;
+            public float currentLength;
+            public float currentVelocity;
         }
 
-        private static readonly WheelType[] _wheelTypes = new WheelType[]
+        public event Action OnVehicleCrashed;
+
+        private static readonly WheelType[] _wheels = new WheelType[]
         {
-            WheelType.BackLeft, WheelType.BackRight, WheelType.FrontLeft, WheelType.FrontRight
+            WheelType.FrontLeft, WheelType.FrontRight, WheelType.BackLeft, WheelType.BackRight
         };
 
-        [SerializeField] private VehicleSettingsSO _vehicleSettings;
-        [SerializeField] private Rigidbody _rigidbody;
-        [SerializeField] private BoxCollider _hitcollider;
-        [SerializeField] private Transform _vehicleTransform;
+        private static readonly WheelType[] _frontWheels = new WheelType[]
+            { WheelType.FrontLeft, WheelType.FrontRight };
 
+        private static readonly WheelType[] _backWheels = new WheelType[] { WheelType.BackLeft, WheelType.BackRight };
+
+        [Header("References")] [SerializeField]
+        private VehicleSettingsSO _vehicleSettings;
+
+        [SerializeField] private Rigidbody _vehicleRigidbody;
+        [SerializeField] private BoxCollider _vehicleCollider;
+
+        [Header("Settings")] [SerializeField] private float _crashForce;
+        [SerializeField] private float _crashTorque;
+
+        private Transform _vehicleTransform;
         private Dictionary<WheelType, SpringData> _springDatas;
+
         private float _steerInput;
-        private float _accelerationInput;
+        private float _accelerateInput;
+
+        public VehicleSettingsSO Settings => _vehicleSettings;
+        public Vector3 Forward => _vehicleTransform.forward;
+        public Vector3 Velocity => _vehicleRigidbody.linearVelocity;
 
         private void Awake()
         {
+            _vehicleTransform = transform;
+
             _springDatas = new Dictionary<WheelType, SpringData>();
-            foreach (WheelType wheelType in _wheelTypes)
+            foreach (WheelType wheel in _wheels)
             {
-                _springDatas.Add(wheelType, new SpringData());
+                _springDatas.Add(wheel, new());
             }
+
+            _vehicleRigidbody.isKinematic = true;
+            SetOwnerRigidbodyKinematicAsync();
         }
 
         private void Update()
         {
             SetSteerInput(Input.GetAxis("Horizontal"));
-            SetAccelerationInput(Input.GetAxis("Vertical"));
+            SetAccelerateInput(Input.GetAxis("Vertical"));
         }
 
         private void FixedUpdate()
         {
             UpdateSuspension();
+            UpdateSteering();
+            UpdateAccelerate();
+            UpdateBrakes();
+            UpdateAirResistance();
+        }
+
+        private async void SetOwnerRigidbodyKinematicAsync()
+        {
+            _vehicleRigidbody.isKinematic = false;
         }
 
         private void SetSteerInput(float steerInput)
         {
-            _steerInput = Mathf.Clamp(steerInput, -1f, 1f);
+            _steerInput = Mathf.Clamp(steerInput, -1.0f, 1.0f);
         }
 
-        private void SetAccelerationInput(float accelerationInput)
+        private void SetAccelerateInput(float accelerateInput)
         {
-            _accelerationInput = Mathf.Clamp(accelerationInput, -1f, 1f);
+            _accelerateInput = Mathf.Clamp(accelerateInput, -1.0f, 1.0f);
         }
 
-        private void UpdateSuspension()
+        public float GetSpringCurrentLength(WheelType wheel)
         {
-            foreach (WheelType id in _springDatas.Keys)
-            {
-                CastSpring(id);
-                float currentVelocity = _springDatas[id]._currentVelocity;
-                float currentLength = _springDatas[id]._currentLength;
-
-                float force = SpringMathExtensions.CalculateForceDamped(currentLength, currentVelocity,
-                    _vehicleSettings.SpringRestLenght, _vehicleSettings.SpringStrenght,
-                    _vehicleSettings.SpringDamper);
-
-                _rigidbody.AddForceAtPosition(force * transform.up, GetSpringPosition(id));
-            }
+            return _springDatas[wheel].currentLength;
         }
 
         private void CastSpring(WheelType wheel)
         {
             Vector3 position = GetSpringPosition(wheel);
 
-            float previousLength = _springDatas[wheel]._currentLength;
+            float previousLength = _springDatas[wheel].currentLength;
 
             float currentLength;
 
-            if (Physics.Raycast(position, -_vehicleTransform.up, out var hit, _vehicleSettings.SpringRestLenght))
+            if (Physics.Raycast(position, -_vehicleTransform.up, out var hit, _vehicleSettings.SpringRestLength))
             {
                 currentLength = hit.distance;
             }
             else
             {
-                currentLength = _vehicleSettings.SpringRestLenght;
+                currentLength = _vehicleSettings.SpringRestLength;
             }
 
-            _springDatas[wheel]._currentVelocity = (currentLength - previousLength) / Time.fixedDeltaTime;
-            _springDatas[wheel]._currentLength = currentLength;
-        } 
-
-        private Vector3 GetSpringPosition(WheelType wheel)
-        {
-            return _vehicleTransform.localToWorldMatrix.MultiplyPoint3x4(GetSpringRelativePosition(wheel));
+            _springDatas[wheel].currentVelocity = (currentLength - previousLength) / Time.fixedDeltaTime;
+            _springDatas[wheel].currentLength = currentLength;
         }
 
         private Vector3 GetSpringRelativePosition(WheelType wheel)
         {
-            Vector3 boxSize = _hitcollider.size;
+            Vector3 boxSize = _vehicleCollider.size;
             float boxBottom = boxSize.y * -0.5f;
 
             float paddingX = _vehicleSettings.WheelsPaddingX;
@@ -108,13 +124,261 @@ namespace Game.Player
 
             return wheel switch
             {
-                WheelType.FrontLeft => new Vector3(boxSize.x * (paddingX - 0.5f), boxBottom, boxSize.z * (0.5f - paddingZ)),
-                WheelType.FrontRight => new Vector3(boxSize.x * (0.5f - paddingX), boxBottom, boxSize.z * (0.5f - paddingZ)),
-                WheelType.BackLeft => new Vector3(boxSize.x * (paddingX - 0.5f), boxBottom, boxSize.z * (paddingZ - 0.5f)),
-                WheelType.BackRight => new Vector3(boxSize.x * (0.5f - paddingX), boxBottom, boxSize.z * (paddingZ - 0.5f)),
+                WheelType.FrontLeft => new Vector3(boxSize.x * (paddingX - 0.5f), boxBottom,
+                    boxSize.z * (0.5f - paddingZ)),
+                WheelType.FrontRight => new Vector3(boxSize.x * (0.5f - paddingX), boxBottom,
+                    boxSize.z * (0.5f - paddingZ)),
+                WheelType.BackLeft => new Vector3(boxSize.x * (paddingX - 0.5f), boxBottom,
+                    boxSize.z * (paddingZ - 0.5f)),
+                WheelType.BackRight => new Vector3(boxSize.x * (0.5f - paddingX), boxBottom,
+                    boxSize.z * (paddingZ - 0.5f)),
                 _ => default,
             };
         }
+
+        private Vector3 GetSpringPosition(WheelType wheel)
+        {
+            return _vehicleTransform.localToWorldMatrix.MultiplyPoint3x4(GetSpringRelativePosition(wheel));
+        }
+
+        private Vector3 GetSpringHitPosition(WheelType wheel)
+        {
+            Vector3 vehicleDown = -_vehicleTransform.up;
+            return GetSpringPosition(wheel) + _springDatas[wheel].currentLength * vehicleDown;
+        }
+
+        private Vector3 GetWheelRollDirection(WheelType wheel)
+        {
+            bool frontWheel = wheel == WheelType.FrontLeft || wheel == WheelType.FrontRight;
+
+            if (frontWheel)
+            {
+                var steerQuaternion = Quaternion.AngleAxis(_steerInput * _vehicleSettings.SteerAngle, Vector3.up);
+                return steerQuaternion * _vehicleTransform.forward;
+            }
+            else
+            {
+                return _vehicleTransform.forward;
+            }
+        }
+
+        private Vector3 GetWheelSlideDirection(WheelType wheel)
+        {
+            Vector3 forward = GetWheelRollDirection(wheel);
+            return Vector3.Cross(_vehicleTransform.up, forward);
+        }
+
+        private Vector3 GetWheelTorqueRelativePosition(WheelType wheel)
+        {
+            Vector3 boxSize = _vehicleCollider.size;
+
+            float paddingX = _vehicleSettings.WheelsPaddingX;
+            float paddingZ = _vehicleSettings.WheelsPaddingZ;
+
+            return wheel switch
+            {
+                WheelType.FrontLeft => new Vector3(boxSize.x * (paddingX - 0.5f), 0.0f, boxSize.z * (0.5f - paddingZ)),
+                WheelType.FrontRight => new Vector3(boxSize.x * (0.5f - paddingX), 0.0f, boxSize.z * (0.5f - paddingZ)),
+                WheelType.BackLeft => new Vector3(boxSize.x * (paddingX - 0.5f), 0.0f, boxSize.z * (paddingZ - 0.5f)),
+                WheelType.BackRight => new Vector3(boxSize.x * (0.5f - paddingX), 0.0f, boxSize.z * (paddingZ - 0.5f)),
+                _ => default,
+            };
+        }
+
+        private Vector3 GetWheelTorquePosition(WheelType wheel)
+        {
+            return _vehicleTransform.localToWorldMatrix.MultiplyPoint3x4(GetWheelTorqueRelativePosition(wheel));
+        }
+
+        private float GetWheelGripFactor(WheelType wheel)
+        {
+            bool frontWheel = wheel == WheelType.FrontLeft || wheel == WheelType.FrontRight;
+            return frontWheel ? _vehicleSettings.FrontWheelsGripFactor : _vehicleSettings.RearWheelsGripFactor;
+        }
+
+        private bool IsGrounded(WheelType wheel)
+        {
+            return _springDatas[wheel].currentLength < _vehicleSettings.SpringRestLength;
+        }
+
+        private void UpdateSuspension()
+        {
+            foreach (WheelType id in _springDatas.Keys)
+            {
+                CastSpring(id);
+                float currentLength = _springDatas[id].currentLength;
+                float currentVelocity = _springDatas[id].currentVelocity;
+
+                float force = SpringMathExtensions.CalculateForceDamped(currentLength, currentVelocity,
+                    _vehicleSettings.SpringRestLength, _vehicleSettings.SpringStrength,
+                    _vehicleSettings.SpringDamper);
+
+                _vehicleRigidbody.AddForceAtPosition(force * _vehicleTransform.up, GetSpringPosition(id));
+            }
+        }
+
+        private void UpdateSteering()
+        {
+            foreach (WheelType wheel in _wheels)
+            {
+                if (!IsGrounded(wheel))
+                {
+                    continue;
+                }
+
+                Vector3 springPosition = GetSpringPosition(wheel);
+
+                Vector3 slideDirection = GetWheelSlideDirection(wheel);
+                float slideVelocity = Vector3.Dot(slideDirection, _vehicleRigidbody.GetPointVelocity(springPosition));
+
+                float desiredVelocityChange = -slideVelocity * GetWheelGripFactor(wheel);
+                float desiredAcceleration = desiredVelocityChange / Time.fixedDeltaTime;
+
+                Vector3 force = desiredAcceleration * _vehicleSettings.TireMass * slideDirection;
+                _vehicleRigidbody.AddForceAtPosition(force, GetWheelTorquePosition(wheel));
+            }
+        }
+
+        private void UpdateAccelerate()
+        {
+            if (Mathf.Approximately(_accelerateInput, 0.0f))
+            {
+                return;
+            }
+
+            float forwardSpeed = Vector3.Dot(_vehicleTransform.forward, _vehicleRigidbody.linearVelocity);
+            bool movingForward = forwardSpeed > 0.0f;
+            float speed = Mathf.Abs(forwardSpeed);
+
+            if (movingForward && speed > _vehicleSettings.MaxSpeed)
+            {
+                return;
+            }
+            else if (!movingForward && speed > _vehicleSettings.MaxReverseSpeed)
+            {
+                return;
+            }
+
+            foreach (WheelType wheel in _wheels)
+            {
+                if (!IsGrounded(wheel))
+                {
+                    continue;
+                }
+
+                Vector3 position = GetWheelTorquePosition(wheel);
+                Vector3 wheelForward = GetWheelRollDirection(wheel);
+                _vehicleRigidbody.AddForceAtPosition(_accelerateInput * _vehicleSettings.AcceleratePower * wheelForward,
+                    position);
+            }
+        }
+
+        private void UpdateBrakes()
+        {
+            float forwardSpeed = Vector3.Dot(_vehicleTransform.forward, _vehicleRigidbody.linearVelocity);
+            float speed = Mathf.Abs(forwardSpeed);
+
+            float brakesRatio;
+
+            const float ALMOST_STOPPING_SPEED = 2.0f;
+            bool almostStopping = speed < ALMOST_STOPPING_SPEED;
+            if (almostStopping)
+            {
+                brakesRatio = 1.0f;
+            }
+            else
+            {
+                bool accelerateContrary =
+                    !Mathf.Approximately(_accelerateInput, 0.0f) &&
+                    Vector3.Dot(_accelerateInput * _vehicleTransform.forward, _vehicleRigidbody.linearVelocity) < 0.0f;
+                if (accelerateContrary)
+                {
+                    brakesRatio = 1.0f;
+                }
+                // NO ACCELERATE INPUT
+                else if (Mathf.Approximately(_accelerateInput, 0.0f))
+                {
+                    brakesRatio = 0.1f;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            foreach (WheelType wheel in _backWheels)
+            {
+                if (!IsGrounded(wheel))
+                {
+                    continue;
+                }
+
+                Vector3 springPosition = GetSpringPosition(wheel);
+                Vector3 rollDirection = GetWheelRollDirection(wheel);
+                float rollVelocity = Vector3.Dot(rollDirection, _vehicleRigidbody.GetPointVelocity(springPosition));
+
+                float desiredVelocityChange = -rollVelocity * _vehicleSettings.BrakesPower * brakesRatio;
+                float desiredAcceleration = desiredVelocityChange / Time.fixedDeltaTime;
+
+                Vector3 force = desiredAcceleration * _vehicleSettings.TireMass * rollDirection;
+                _vehicleRigidbody.AddForceAtPosition(force, GetWheelTorquePosition(wheel));
+            }
+        }
+
+        private void UpdateAirResistance()
+        {
+            _vehicleRigidbody.AddForce(_vehicleCollider.size.magnitude * _vehicleSettings.AirResistance *
+                                       -_vehicleRigidbody.linearVelocity);
+        }
+
+        public void CrashVehicle()
+        {
+            OnVehicleCrashed?.Invoke();
+
+            _vehicleRigidbody.AddForce(Vector3.up * _crashForce, ForceMode.Impulse);
+            _vehicleRigidbody.AddTorque(Vector3.forward * _crashTorque, ForceMode.Impulse);
+            enabled = false;
+        }
+
+        public void OnPlayerRespawned() => enabled = true;
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            if (Application.isPlaying)
+            {
+                Vector3 vehicleDown = -transform.up;
+
+                foreach (WheelType wheel in _springDatas.Keys)
+                {
+                    // Spring
+                    Vector3 position = GetSpringPosition(wheel);
+                    Gizmos.color = Color.yellow;
+                    Gizmos.DrawLine(position, position + vehicleDown * _vehicleSettings.SpringRestLength);
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawCube(GetSpringHitPosition(wheel), Vector3.one * 0.08f);
+
+                    // Wheel
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawRay(position, GetWheelRollDirection(wheel));
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawRay(position, GetWheelSlideDirection(wheel));
+                }
+            }
+            else
+            {
+                if (_vehicleSettings != null)
+                {
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawWireCube(transform.position,
+                        new Vector3(
+                            _vehicleSettings.Width,
+                            _vehicleSettings.Height,
+                            _vehicleSettings.Length));
+                }
+            }
+        }
+#endif
     }
 
     public static class SpringMathExtensions
--- Assets/_Projects/Scripts/Player/PlayerVehicleVisual.cs
+++ Assets/_Projects/Scripts/Player/PlayerVehicleVisual.cs
@@ -1,9 +1,180 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace Game.Player
 {
     public class PlayerVehicleVisual : MonoBehaviour
     {
-    
+        [SerializeField] private Transform _jeepVisualTransform; 
+        [SerializeField] private Collider _playerCollider;
+        [SerializeField] private Transform _wheelFrontLeft, _wheelFrontRight, _wheelBackLeft, _wheelBackRight;
+        [SerializeField] private float _wheelsSpinSpeed, _wheelYWhenSpringMin, _wheelYWhenSpringMax;
+        [SerializeField] private ParticleSystem[] _smokeParticles;
+
+        private PlayerVehicleController _playerVehicleController;
+        private Quaternion _wheelFrontLeftRoll;
+        private Quaternion _wheelFrontRightRoll;
+
+        private float _forwardSpeed;
+        private float _steerInput;
+        private float _steerAngle;
+        private float _springsRestLength;
+
+        private Dictionary<WheelType, float> _springsCurrentLength = new()
+        {
+            { WheelType.FrontLeft, 0.0f },
+            { WheelType.FrontRight, 0.0f },
+            { WheelType.BackLeft, 0.0f },
+            { WheelType.BackRight, 0.0f }
+        };
+
+        private void Awake()
+        {
+            Spawn();
+        }
+
+        private void OnEnable()
+        {
+            _playerVehicleController.OnVehicleCrashed += PlayerVehicleController_OnVehicleCrashed;
+        }
+
+        private void OnDisable()
+        {
+            _playerVehicleController.OnVehicleCrashed -= PlayerVehicleController_OnVehicleCrashed;
+        }
+
+        private void Spawn()
+        {  
+            _playerVehicleController = GetComponent<PlayerVehicleController>();
+
+            _wheelFrontLeftRoll = _wheelFrontLeft.localRotation;
+            _wheelFrontRightRoll = _wheelFrontRight.localRotation;
+
+            _springsRestLength = _playerVehicleController.Settings.SpringRestLength;
+            _steerAngle = _playerVehicleController.Settings.SteerAngle; 
+        }
+
+        private void Update()
+        { 
+            UpdateVisualStates();
+            RotateWheels();
+            SetSuspension();
+        }
+
+        private void PlayerVehicleController_OnVehicleCrashed()
+        {
+            enabled = false;
+        }
+
+        private void RotateWheels()
+        {
+            if (_springsCurrentLength[WheelType.FrontLeft] < _springsRestLength)
+            {
+                _wheelFrontLeftRoll *=
+                    Quaternion.AngleAxis(_forwardSpeed * _wheelsSpinSpeed * Time.deltaTime, Vector3.right);
+            }
+
+            if (_springsCurrentLength[WheelType.FrontRight] < _springsRestLength)
+            {
+                _wheelFrontRightRoll *=
+                    Quaternion.AngleAxis(_forwardSpeed * _wheelsSpinSpeed * Time.deltaTime, Vector3.right);
+            }
+
+            if (_springsCurrentLength[WheelType.BackLeft] < _springsRestLength)
+            {
+                _wheelBackLeft.localRotation *=
+                    Quaternion.AngleAxis(_forwardSpeed * _wheelsSpinSpeed * Time.deltaTime, Vector3.right);
+            }
+
+            if (_springsCurrentLength[WheelType.BackRight] < _springsRestLength)
+            {
+                _wheelBackRight.localRotation *=
+                    Quaternion.AngleAxis(_forwardSpeed * _wheelsSpinSpeed * Time.deltaTime, Vector3.right);
+            }
+
+            _wheelFrontLeft.localRotation =
+                Quaternion.AngleAxis(_steerInput * _steerAngle, Vector3.up) * _wheelFrontLeftRoll;
+            _wheelFrontRight.localRotation =
+                Quaternion.AngleAxis(_steerInput * _steerAngle, Vector3.up) * _wheelFrontRightRoll;
+        }
+
+        private void SetSuspension()
+        {
+            float springFrontLeftRatio = _springsCurrentLength[WheelType.FrontLeft] / _springsRestLength;
+            float springFrontRightRatio = _springsCurrentLength[WheelType.FrontRight] / _springsRestLength;
+            float springBackLeftRatio = _springsCurrentLength[WheelType.BackLeft] / _springsRestLength;
+            float springBackRightRatio = _springsCurrentLength[WheelType.BackRight] / _springsRestLength;
+
+            _wheelFrontLeft.localPosition = new Vector3(_wheelFrontLeft.localPosition.x,
+                _wheelYWhenSpringMin + (_wheelYWhenSpringMax - _wheelYWhenSpringMin) * springFrontLeftRatio,
+                _wheelFrontLeft.localPosition.z);
+
+            _wheelFrontRight.localPosition = new Vector3(_wheelFrontRight.localPosition.x,
+                _wheelYWhenSpringMin + (_wheelYWhenSpringMax - _wheelYWhenSpringMin) * springFrontRightRatio,
+                _wheelFrontRight.localPosition.z);
+
+            _wheelBackRight.localPosition = new Vector3(_wheelBackRight.localPosition.x,
+                _wheelYWhenSpringMin + (_wheelYWhenSpringMax - _wheelYWhenSpringMin) * springBackRightRatio,
+                _wheelBackRight.localPosition.z);
+
+            _wheelBackLeft.localPosition = new Vector3(_wheelBackLeft.localPosition.x,
+                _wheelYWhenSpringMin + (_wheelYWhenSpringMax - _wheelYWhenSpringMin) * springBackLeftRatio,
+                _wheelBackLeft.localPosition.z);
+        }
+
+        private void UpdateVisualStates()
+        {
+            _steerInput = Input.GetAxis("Horizontal");
+
+            float forwardSpeed = Vector3.Dot(_playerVehicleController.Forward, _playerVehicleController.Velocity);
+            _forwardSpeed = forwardSpeed;
+
+            _springsCurrentLength[WheelType.FrontLeft] =
+                _playerVehicleController.GetSpringCurrentLength(WheelType.FrontLeft);
+            _springsCurrentLength[WheelType.FrontRight] =
+                _playerVehicleController.GetSpringCurrentLength(WheelType.FrontRight);
+            _springsCurrentLength[WheelType.BackLeft] =
+                _playerVehicleController.GetSpringCurrentLength(WheelType.BackLeft);
+            _springsCurrentLength[WheelType.BackRight] =
+                _playerVehicleController.GetSpringCurrentLength(WheelType.BackRight);
+        }
+
+
+        [Rpc(SendTo.ClientsAndHost)]
+        private void SetJeepVisualActiveRpc(bool isActive)
+        {
+            _jeepVisualTransform.gameObject.SetActive(isActive);
+        }
+
+        private IEnumerator SetVehicleVisualActiveCoroutine(float delay)
+        {
+            SetJeepVisualActiveRpc(false);
+            _playerCollider.enabled = false;
+
+            yield return new WaitForSeconds(delay);
+
+            SetJeepVisualActiveRpc(true);
+            _playerCollider.enabled = true;
+            enabled = true;
+            RestartSmokeParticlesRpc();
+        }
+
+        [Rpc(SendTo.ClientsAndHost)]
+        private void RestartSmokeParticlesRpc()
+        {
+            foreach (ParticleSystem smokeParticle in _smokeParticles)
+            {
+                smokeParticle.Stop();
+                smokeParticle.Play();
+            }
+        }
+
+        public void SetVehicleVisualActive(float delay)
+        {
+            StartCoroutine(SetVehicleVisualActiveCoroutine(delay));
+        } 
     }
 }
