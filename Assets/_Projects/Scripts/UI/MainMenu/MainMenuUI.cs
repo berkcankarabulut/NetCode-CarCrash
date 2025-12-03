c2bdab94549da143c6f92a8a986ea0343944327c
Berkcan Karabulut <46574515+berkcankarabulut@users.noreply.github.com>
2025-12-03 17:58:20 +0300

    v0.15: Lobby UI eklendi. Açılıp kapanma animasyonu eklendi.

--- Assets/_Projects/Scripts/UI/MainMenu/MainMenuUI.cs
+++ Assets/_Projects/Scripts/UI/MainMenu/MainMenuUI.cs
@@ -1,5 +1,6 @@
 using _Project.Networking.Client;
 using _Project.Networking.Host;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,18 +11,32 @@ namespace _Project.UI.MainMenu
     {
         [SerializeField] private Button _hostButton;
         [SerializeField] private Button _clientButton;
+        [SerializeField] private Button _lobbiesButton;
+        [SerializeField] private Button _closeLobbyButton;
+        [SerializeField] private Button _refreshButton;
+        [SerializeField] private RectTransform _lobbiesTransform;
         [SerializeField] private TMP_InputField _hostInputField;
+
+        [SerializeField] private float _animationDuration = 0.5f;
+
         private void OnEnable()
         {
             _hostButton.onClick.AddListener(StartHost);
             _clientButton.onClick.AddListener(StartClient);
+            _lobbiesButton.onClick.AddListener(OpenLobbies);
+            _closeLobbyButton.onClick.AddListener(CloseLobbies);
+            _refreshButton.onClick.AddListener(RefreshLobbies);
         }
 
         private void OnDisable()
         {
             _hostButton.onClick.RemoveListener(StartHost);
             _clientButton.onClick.RemoveListener(StartClient);
+            _lobbiesButton.onClick.RemoveListener(OpenLobbies);
+            _closeLobbyButton.onClick.RemoveListener(CloseLobbies);
+            _refreshButton.onClick.RemoveListener(RefreshLobbies);
         }
+
         private async void StartHost()
         {
             _hostButton.interactable = false;
@@ -31,6 +46,22 @@ namespace _Project.UI.MainMenu
         private async void StartClient()
         {
             await ClientSingleton.Instance.ClientManager.StartClientAsync(_hostInputField.text);
+        }
+
+
+        private void OpenLobbies()
+        { 
+            _lobbiesTransform.DOAnchorPosX(-650f, _animationDuration).SetEase(Ease.OutBack);
+        }
+
+        private void RefreshLobbies()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void CloseLobbies()
+        { 
+            _lobbiesTransform.DOAnchorPosX(900, _animationDuration).SetEase(Ease.OutBack);
         }
     }
 }
