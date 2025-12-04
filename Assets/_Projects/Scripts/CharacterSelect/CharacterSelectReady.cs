cb54f7fc55c467a05c6f975372d2c7f33ad255d5
Berkcan Karabulut <46574515+berkcankarabulut@users.noreply.github.com>
2025-12-04 16:45:19 +0300

    v0.17[FEATURE]:  CharacterSelectReady ekranı için Multiplayer tarafındaki Ready/Unready check yapısı oluşturuldu.

---
+++ Assets/_Projects/Scripts/CharacterSelect/CharacterSelectReady.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using Unity.Netcode;
+
+namespace _Project.CharacterSelect
+{
+    public class CharacterSelectReady : NetworkBehaviour
+    {
+        public event Action OnReadyChanged;
+        public event Action OnUnReadyChanged;
+        public event Action OnAllReadyChanged;
+
+        private Dictionary<ulong, bool> _playerReady = new Dictionary<ulong, bool>();
+
+        public override void OnNetworkSpawn()
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallBack;
+            NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnectCallBack;
+        }
+
+        private void OnClientConnectedCallBack(ulong connectedClientId)
+        {
+            foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+            {
+                if (IsPlayerReady(clientId))
+                {
+                    SetPlayerReadyToAllRpc(clientId);
+                }
+            }
+        }
+
+        private void OnClientDisconnectCallBack(ulong clientId)
+        {
+            if (_playerReady.ContainsKey(clientId))
+            {
+                _playerReady.Remove(clientId);
+                OnUnReadyChanged?.Invoke();
+            }
+        }
+
+        [Rpc(SendTo.Server)]
+        private void SetPlayerReadyRpc(RpcParams rpcParams = default)
+        {
+            SetPlayerReadyToAllRpc(rpcParams.Receive.SenderClientId);
+            _playerReady[rpcParams.Receive.SenderClientId] = true;
+            bool allClientsReady = true;
+
+            foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+            {
+                if (_playerReady.ContainsKey(clientId) && _playerReady[clientId]) continue;
+                allClientsReady = false;
+                break;
+            }
+
+            if (allClientsReady)
+            {
+                OnAllReadyChanged?.Invoke();
+            }
+        }
+
+        [Rpc(SendTo.Server)]
+        private void SetPlayerUnReadyRpc(RpcParams rpcParams = default)
+        {
+            SetPlayerUnReadyToAllRpc(rpcParams.Receive.SenderClientId);
+            if (_playerReady.ContainsKey(rpcParams.Receive.SenderClientId))
+            {
+                _playerReady[rpcParams.Receive.SenderClientId] = false;
+            }
+        }
+
+        [Rpc(SendTo.ClientsAndHost)]
+        private void SetPlayerReadyToAllRpc(ulong clientId)
+        {
+            _playerReady[clientId] = true;
+            OnReadyChanged?.Invoke();
+        }
+
+        [Rpc(SendTo.ClientsAndHost)]
+        private void SetPlayerUnReadyToAllRpc(ulong clientId)
+        {
+            _playerReady[clientId] = false;
+            OnReadyChanged?.Invoke();
+            OnUnReadyChanged?.Invoke();
+        }
+
+        public bool IsPlayerReady(ulong playerId)
+        {
+            return _playerReady.ContainsKey(playerId) && _playerReady[playerId];
+        }
+
+        public bool AreAllPlayersReady(ulong playerId)
+        {
+            foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+            {
+                if (_playerReady.ContainsKey(clientId) && _playerReady[clientId]) continue;
+                return false;
+            } 
+            return true;
+        }
+
+        public void SetPlayerReady()
+        {
+            SetPlayerReadyRpc();
+        }
+
+        public void SetPlayerUnReady()
+        {
+            SetPlayerUnReadyRpc();
+        }
+    }
+}
